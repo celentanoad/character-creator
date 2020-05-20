5ddc4be57840fd6116f6981bd14ab1f15eefe5aa
Alanna Celentano <alannacelentano@Alannas-MacBook-Pro.local>
2020-05-20 15:09:50 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <nav>
+            <p>Character Creator using the DnD API</p>
+        </nav>
+     );
+}
+ 
+export default Header;
